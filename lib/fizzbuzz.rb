8c44cc194ce8506ccc30e871253c0ab19323fe3e
Imogen Misso <imogenmisso@hotmail.co.uk>
2018-09-21 14:29:36 +0100

    refactoring

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,13 +1,8 @@
 def fizzbuzz(number)
-  if number % 5 == 0 && number % 3 == 0
-    return 'fizzbuzz'
-  elsif number % 3 == 0
-    return 'fizz'
-  elsif number % 5 == 0
-    return 'buzz'
-  else
-    return number
-  end
+  return 'fizzbuzz' if number % 5 == 0 && number % 3 == 0
+  return 'fizz' if number % 3 == 0
+  return 'buzz' if number % 5 == 0
+  return number else
 end
 
 #to test in irb => type (1..100).each do |i|, puts fizzbuzz(i), end
